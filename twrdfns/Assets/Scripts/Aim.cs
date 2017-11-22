efe1f1fd96462bb4fee04a415c61a81c0842c2a6
Berk Erkul <berk.erkul@gmail.com>
2017-11-21 20:28:41 -0800

    Aim and fire

---
+++ twrdfns/Assets/Scripts/Aim.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour {
+
+    private Vector3 pos;
+    private Camera cam;
+
+    // Use this for initialization
+    void Start () {
+        pos = new Vector3();
+        cam = Camera.main;
+        CalcScreenToWorldPos(0f, 0f);        
+	}
+	
+	// Update is called once per frame
+	void Update () {        
+        transform.LookAt(pos);	}
+
+    private void OnGUI()
+    {   
+        Event e = Event.current;        
+        CalcScreenToWorldPos(e.mousePosition.x, e.mousePosition.y);
+    }
+
+    private void CalcScreenToWorldPos(float x, float y)
+    {
+        if (cam == null)
+        {
+            cam = Camera.main;
+        }
+
+        pos = cam.ScreenToWorldPoint(new Vector3(x, y, 100f));
+        pos.y = transform.position.y;
+
+    }
+}
---
+++ twrdfns/Assets/Scripts/Fire.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+
+    
+    public Rigidbody shell;
+    public float speed = 20f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        		
+	}
+
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FireShell();
+        }
+    }
+
+    void FireShell()
+    {
+        Rigidbody shellClone = (Rigidbody)Instantiate(shell, transform.position, transform.rotation);
+        shellClone.velocity = transform.forward * speed;
+    }
+}
